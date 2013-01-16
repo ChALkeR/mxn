ChALkeR/mxn

commit 6daf742a5482eedac8552988a9abd15ac1b4cbc2

Manual merge of Pull Request #202 from @gilesc50 (Added optional parameter to request multiple results from the geocoder API)

source/mxn.openlayers.geocoder.js
@@ -1,18 +1,18 @@
-mxn.register('openlayers', {	
+mxn.register('openlayers', {
 
 Geocoder: {
-	
+
 	init: function() {
 		var me = this;
 	},
-	
-	geocode: function(address){
+
+	geocode: function(address, rowlimit) {
 		var me = this;
-		
+		rowlimit = rowlimit || 1; //default to one result
 		if (!address.hasOwnProperty('address') || address.address === null || address.address === '') {
 			address.address = [ address.street, address.locality, address.region, address.country ].join(', ');
 		}
-		
+
 		if (address.hasOwnProperty('lat') && address.hasOwnProperty('lon')) {
 			var latlon = address.toProprietary(this.api);
 			OpenLayers.Request.GET({
@@ -23,9 +23,10 @@ Geocoder: {
 					'addressdetails': 1,
 					'format': 'json'
 				},
-				callback: function(request) { me.geocode_callback(JSON.parse(request.responseText), request.status); }
+				callback: function(request) { me.geocode_callback(JSON.parse(request.responseText), request.status, rowlimit); }
 			});
-		} else {
+		}
+		else {
 			OpenLayers.Request.GET({
 				url: 'http://nominatim.openstreetmap.org/search',
 				params: {
@@ -33,63 +34,86 @@ Geocoder: {
 					'addressdetails': 1,
 					'format': 'json'
 				},
-				callback: function(request) { me.geocode_callback(JSON.parse(request.responseText), request.status); }
+				callback: function(request) { me.geocode_callback(JSON.parse(request.responseText), request.status, rowlimit); }
 			});
 		}
 	},
-	
-	geocode_callback: function(results, status){
-		var return_location = {};
-		
+
+	geocode_callback: function(results, status, rowlimit) {
+		console.log('geocode_callback: ' + status);
 		if (status != 200) {
 			this.error_callback(response.statusText);
 		}
-		else if (results instanceof Array && !results.length){
+		else if (results instanceof Array && !results.length) {
 			this.error_callback("OpenLayers didn't recognize this address.");
 		}
 		else {
-			return_location.street = '';
-			return_location.locality = '';
-			return_location.postcode = '';
-			return_location.region = '';
-			return_location.country = '';
-			
 			var place;
-			
-			if (results.length > 0) {
-				place = results[0];
-			} else {
-				place = results;
+			var places = [];
+
+			console.log ('Got ' + results.length + ' results');
+			for (i=0; i<results.length; i++) {
+				place = results[i];
+				var return_location = {};
+				return_location.street = '';
+				return_location.locality = '';
+				return_location.postcode = '';
+				return_location.region = '';
+				return_location.country = '';
+				var street_components = [];
+
+				console.log (JSON.stringify(place));
+				
+				if (place.address.country) {
+					return_location.country = place.address.country;
+				}
+				if (place.address.state) {
+					return_location.region = place.address.state;
+				}
+				if (place.address.city) {
+					return_location.locality = place.address.city;
+				}
+				else if (place.address.town) {
+					return_location.locality = place.address.town;
+				}
+				else if (place.address.village) {
+					return_location.locality = place.address.village;
+				}
+				else if (place.address.hamlet) {
+					return_location.locality = place.address.hamlet;
+				}
+
+				if (place.address.postcode) {
+					return_location.postcode = place.address.postcode;
+				}
+				if (place.address.road) {
+					street_components.push(place.address.road);
+				}
+				if (place.address.house_number) {
+					street_components.unshift(place.address.house_number);
+				}
+
+				if (return_location.street === '' && street_components.length > 0) {
+					return_location.street = street_components.join(' ');
+				}
+
+				return_location.point = new mxn.LatLonPoint(parseFloat(place.lat), parseFloat(place.lon));
+
+				places.push(return_location);
 			}
-			var street_components = [];
-			
-			if (place.address.country) {
-				return_location.country = place.address.country;
+
+			if (rowlimit <= 1) {
+				this.callback(places[0]);
 			}
-			if (place.address.state) {
-				return_location.region = place.address.state;
+
+			else {
+				if (places.length > rowlimit) {
+					places.length = rowlimit;
+				}
+				this.callback(places);
 			}
-			if (place.address.city) {
-				return_location.locality = place.address.city;
-			}
-			if (place.address.postcode) {
-				return_location.postcode = place.address.postcode;
-			}
-			if (place.address.road) {
-				street_components.push(place.address.road);
-			}
-			if (place.address.house_number) {
-				street_components.unshift(place.address.house_number);
-			}
-			
-			if (return_location.street === '' && street_components.length > 0) {
-				return_location.street = street_components.join(' ');
-			}
-			
-			return_location.point = new mxn.LatLonPoint(parseFloat(place.lat), parseFloat(place.lon));
-			
-			this.callback(return_location);
 		}
 	}
 }
+
 });

examples/googlev3-geocoder.html
@@ -1,12 +1,14 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>Mapstraction Examples - OpenLayers Geocoder</title>
-<script src="http://openlayers.org/api/2.11/OpenLayers.js"></script>
-<script src="../source/mxn.js?(openlayers,[geocoder])" type="text/javascript"></script>
+<title>Mapstraction Examples - googlev3 Geocoder</title>
+	<script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript" charset="utf-8"></script>
+<script src="../source/mxn.js?(googlev3,[geocoder])" type="text/javascript"></script>
 <style type="text/css">
 
 	#mapdiv {
 		height: 400px;
+		position: relative;
+		width: !important;
 	}
 
 </style> 
@@ -17,41 +19,35 @@
 	var geocoder;
 
 	function geocode_return(geocoded_locations) {
-		console.log ('test: geocode_return++');
-		if (typeof(geocoded_locations.point) == "object") { //ie we only got 1 result back the way it used to work
+		if (typeof(geocoded_locations.point) == "object") //ie we only got 1 result back the way it used to work
+		{
 			geocoded_locations_array = {};
 			geocoded_locations_array[0] = geocoded_locations;
 			geocoded_locations = geocoded_locations_array;
 			geocoded_locations.length = 1;
 		}
 
-		console.log ('test: Got ' + geocoded_locations.length + ' results');
-		
 		mapstraction.removeAllMarkers();
-		for (i=0; i<geocoded_locations.length; i++) {
+		for (i=0; i<geocoded_locations.length; i++)
+		{
 			// create a marker positioned at a lat/lon
-			console.log ('test: Result ' + i);
-			console.log ('test: ' + JSON.stringify(geocoded_locations[i]));
-			
 			var geocode_marker = new mxn.Marker(geocoded_locations[i].point);
 			geocode_marker.setIcon('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + i + '|FF1CAE|000000');
 			geocode_marker.setIconSize([21,34]);
 			geocode_marker.setIconAnchor([11,34]);
 			var address = geocoded_locations[i].locality + ", " + geocoded_locations[i].region + ", " + geocoded_locations[i].country;
-geocode_marker.setInfoBubble(address);
-
+			geocode_marker.setInfoBubble(address);
+			
 			// display marker
 			mapstraction.addMarker(geocode_marker);
-			// open the marker
-			//geocode_marker.openBubble();
-		}
+		}	
 		mapstraction.autoCenterAndZoom()
 	}
 
 	function initialize() {
 
 		// create mxn object
-		mapstraction = new mxn.Mapstraction('mapdiv','openlayers');
+		mapstraction = new mxn.Mapstraction('mapdiv','googlev3');
 
 		mapstraction.addControls({
 			pan: true,
@@ -64,7 +60,7 @@ geocode_marker.setInfoBubble(address);
 		mapstraction.setCenterAndZoom(latlon, 10);
 		mapstraction.mousePosition('position');
 		
-		geocoder = new mxn.Geocoder('openlayers',geocode_return);
+		geocoder = new mxn.Geocoder('googlev3',geocode_return);
 		
 		// add a marker
 		var marker = new mxn.Marker(latlon);
@@ -73,8 +69,7 @@ geocode_marker.setInfoBubble(address);
 	}
 
 	function user_submit() {
-		var address = {};
-		address.address = document.getElementById('address').value;
+		var address = document.getElementById('address').value;
 		var limit = document.getElementById('limit').value;
 		geocoder.geocode(address, limit);
 	}
@@ -94,8 +89,8 @@ geocode_marker.setInfoBubble(address);
 				<td>
 					<input type="text" id="address" name="address" value="brest" size="35"/>
 					<a href="#" onclick="user_submit(); return false;">Geocode</a>
-					but limit the number of results to:
-					<input type="text" id="limit" name="limit" value="3" size="1" />
+					but limit the number of results to: 
+					<input type="text" id="limit" name="limit" value="3" size="1"/>					
 				</td>
 			</tr>
 		</table>

examples/microsoft7-geocoder.html
@@ -1,12 +1,14 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>Mapstraction Examples - OpenLayers Geocoder</title>
-<script src="http://openlayers.org/api/2.11/OpenLayers.js"></script>
-<script src="../source/mxn.js?(openlayers,[geocoder])" type="text/javascript"></script>
+<title>Mapstraction Examples - microsoft7 Geocoder</title>
+<script charset="UTF-8" type="text/javascript" src="http://ecn.dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=7.0"></script>		
+<script src="../source/mxn.js?(microsoft7,[geocoder])" type="text/javascript"></script>
 <style type="text/css">
 
 	#mapdiv {
 		height: 400px;
+		position: relative;
+		width: !important;
 	}
 
 </style> 
@@ -15,43 +17,38 @@
 
 	var mapstraction;
 	var geocoder;
+	var microsoft_key = "AnSU6Nk1wZm1jU0z_Jnib5uy6ZdCllBorytKk0BBxJY2rBHVZayodUR5DxqLHlzt";
 
 	function geocode_return(geocoded_locations) {
-		console.log ('test: geocode_return++');
-		if (typeof(geocoded_locations.point) == "object") { //ie we only got 1 result back the way it used to work
+		if (typeof(geocoded_locations.point) == "object") //ie we only got 1 result back the way it used to work
+		{
 			geocoded_locations_array = {};
 			geocoded_locations_array[0] = geocoded_locations;
 			geocoded_locations = geocoded_locations_array;
 			geocoded_locations.length = 1;
 		}
 
-		console.log ('test: Got ' + geocoded_locations.length + ' results');
-		
 		mapstraction.removeAllMarkers();
-		for (i=0; i<geocoded_locations.length; i++) {
+		for (i=0; i<geocoded_locations.length; i++)
+		{
 			// create a marker positioned at a lat/lon
-			console.log ('test: Result ' + i);
-			console.log ('test: ' + JSON.stringify(geocoded_locations[i]));
-			
 			var geocode_marker = new mxn.Marker(geocoded_locations[i].point);
 			geocode_marker.setIcon('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + i + '|FF1CAE|000000');
 			geocode_marker.setIconSize([21,34]);
 			geocode_marker.setIconAnchor([11,34]);
 			var address = geocoded_locations[i].locality + ", " + geocoded_locations[i].region + ", " + geocoded_locations[i].country;
-geocode_marker.setInfoBubble(address);
-
+			geocode_marker.setInfoBubble(address);
+			
 			// display marker
 			mapstraction.addMarker(geocode_marker);
-			// open the marker
-			//geocode_marker.openBubble();
-		}
+		}	
 		mapstraction.autoCenterAndZoom()
 	}
 
 	function initialize() {
 
 		// create mxn object
-		mapstraction = new mxn.Mapstraction('mapdiv','openlayers');
+		mapstraction = new mxn.Mapstraction('mapdiv','microsoft7');
 
 		mapstraction.addControls({
 			pan: true,
@@ -64,7 +61,7 @@ geocode_marker.setInfoBubble(address);
 		mapstraction.setCenterAndZoom(latlon, 10);
 		mapstraction.mousePosition('position');
 		
-		geocoder = new mxn.Geocoder('openlayers',geocode_return);
+		geocoder = new mxn.Geocoder('microsoft7',geocode_return);
 		
 		// add a marker
 		var marker = new mxn.Marker(latlon);
@@ -73,8 +70,7 @@ geocode_marker.setInfoBubble(address);
 	}
 
 	function user_submit() {
-		var address = {};
-		address.address = document.getElementById('address').value;
+		var address = document.getElementById('address').value;
 		var limit = document.getElementById('limit').value;
 		geocoder.geocode(address, limit);
 	}
@@ -94,8 +90,8 @@ geocode_marker.setInfoBubble(address);
 				<td>
 					<input type="text" id="address" name="address" value="brest" size="35"/>
 					<a href="#" onclick="user_submit(); return false;">Geocode</a>
-					but limit the number of results to:
-					<input type="text" id="limit" name="limit" value="3" size="1" />
+					but limit the number of results to: 
+					<input type="text" id="limit" name="limit" value="3" size="1"/>					
 				</td>
 			</tr>
 		</table>

examples/ovi-geocoder.html
@@ -1,12 +1,14 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>Mapstraction Examples - OpenLayers Geocoder</title>
-<script src="http://openlayers.org/api/2.11/OpenLayers.js"></script>
-<script src="../source/mxn.js?(openlayers,[geocoder])" type="text/javascript"></script>
+<title>Mapstraction Examples - ovi Geocoder</title>
+<script src="http://api.maps.ovi.com/jsl.js" type="text/javascript" charset="utf-8"></script>
+<script src="../source/mxn.js?(ovi,[geocoder])" type="text/javascript"></script>
 <style type="text/css">
 
 	#mapdiv {
 		height: 400px;
+		position: relative;
+		width: !important;
 	}
 
 </style> 
@@ -17,41 +19,35 @@
 	var geocoder;
 
 	function geocode_return(geocoded_locations) {
-		console.log ('test: geocode_return++');
-		if (typeof(geocoded_locations.point) == "object") { //ie we only got 1 result back the way it used to work
+		if (typeof(geocoded_locations.point) == "object") //ie we only got 1 result back the way it used to work
+		{
 			geocoded_locations_array = {};
 			geocoded_locations_array[0] = geocoded_locations;
 			geocoded_locations = geocoded_locations_array;
 			geocoded_locations.length = 1;
 		}
 
-		console.log ('test: Got ' + geocoded_locations.length + ' results');
-		
 		mapstraction.removeAllMarkers();
-		for (i=0; i<geocoded_locations.length; i++) {
+		for (i=0; i<geocoded_locations.length; i++)
+		{
 			// create a marker positioned at a lat/lon
-			console.log ('test: Result ' + i);
-			console.log ('test: ' + JSON.stringify(geocoded_locations[i]));
-			
 			var geocode_marker = new mxn.Marker(geocoded_locations[i].point);
 			geocode_marker.setIcon('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + i + '|FF1CAE|000000');
 			geocode_marker.setIconSize([21,34]);
 			geocode_marker.setIconAnchor([11,34]);
 			var address = geocoded_locations[i].locality + ", " + geocoded_locations[i].region + ", " + geocoded_locations[i].country;
-geocode_marker.setInfoBubble(address);
-
+			geocode_marker.setInfoBubble(address);
+			
 			// display marker
 			mapstraction.addMarker(geocode_marker);
-			// open the marker
-			//geocode_marker.openBubble();
-		}
+		}	
 		mapstraction.autoCenterAndZoom()
 	}
 
 	function initialize() {
 
 		// create mxn object
-		mapstraction = new mxn.Mapstraction('mapdiv','openlayers');
+		mapstraction = new mxn.Mapstraction('mapdiv','ovi');
 
 		mapstraction.addControls({
 			pan: true,
@@ -64,7 +60,7 @@ geocode_marker.setInfoBubble(address);
 		mapstraction.setCenterAndZoom(latlon, 10);
 		mapstraction.mousePosition('position');
 		
-		geocoder = new mxn.Geocoder('openlayers',geocode_return);
+		geocoder = new mxn.Geocoder('ovi',geocode_return);
 		
 		// add a marker
 		var marker = new mxn.Marker(latlon);
@@ -73,8 +69,7 @@ geocode_marker.setInfoBubble(address);
 	}
 
 	function user_submit() {
-		var address = {};
-		address.address = document.getElementById('address').value;
+		var address = document.getElementById('address').value;
 		var limit = document.getElementById('limit').value;
 		geocoder.geocode(address, limit);
 	}
@@ -94,8 +89,8 @@ geocode_marker.setInfoBubble(address);
 				<td>
 					<input type="text" id="address" name="address" value="brest" size="35"/>
 					<a href="#" onclick="user_submit(); return false;">Geocode</a>
-					but limit the number of results to:
-					<input type="text" id="limit" name="limit" value="3" size="1" />
+					but limit the number of results to: 
+					<input type="text" id="limit" name="limit" value="3" size="1"/>					
 				</td>
 			</tr>
 		</table>

source/mxn.geocoder.js
@@ -13,12 +13,12 @@ var init = function() {
  * Geocoder instantiates a geocoder with some API choice
  * @name mxn.Geocoder
  * @constructor
- * @param {String} api The API to use, currently only 'mapquest' is supported
+ * @param {String} api The API to use
  * @param {Function} callback The function to call when a geocode request returns (function(waypoint))
  * @param {Function} error_callback The optional function to call when a geocode request fails
  * @exports Geocoder as mxn.Geocoder
  */
-var Geocoder = mxn.Geocoder = function (api, callback, error_callback) {
+var Geocoder = mxn.Geocoder = function (api, callback, error_callback, row_limit) {
 	this.api = api;
 	this.geocoders = {};
 	this.callback = callback;
@@ -36,6 +36,7 @@ mxn.addProxyMethods(Geocoder, [
 	 * @name mxn.Geocoder#geocode
 	 * @function
 	 * @param {Object} address Address hash, keys are: street, locality, region, country.
+	 * @param {Int} Row_Limit to limit returned results, defaults to 1 to support previous Mapstraction 2.0 API definition.
 	 */
 	'geocode',
 	

source/mxn.googlev3.geocoder.js
@@ -1,13 +1,15 @@
-mxn.register('googlev3', {	
+mxn.register('googlev3', {
 
 Geocoder: {
-	
 	init: function() {
 		this.geocoders[this.api] = new google.maps.Geocoder();
 	},
-	
-	geocode: function(query){
+
+	row_limit: int = 1,
+
+	geocode: function(query, rowlimit){
 		var me = this;
+		me.row_limit = rowlimit || 1; //default to one result
 		var geocode_request_object = {};
 		if (typeof(query) == 'object') {
 			// query is a LatLonPoint object (reverse geocode)
@@ -19,69 +21,80 @@ Geocoder: {
 				geocode_request_object.address = [ query.street, query.locality, query.region, query.country ].join(', ');
 			}
 		}
+
 		// query is an address string
 		else {
 			geocode_request_object.address = query;
 		}
+
 		this.geocoders[this.api].geocode(geocode_request_object, function(results, status) {
 			me.geocode_callback(results, status);
 		});
 	},
-	
-	geocode_callback: function(results, status){
-		var return_location = {};
 
+	geocode_callback: function(results, status){
 		if (status != google.maps.GeocoderStatus.OK) {
 			this.error_callback(status);
 		} 
-		else {
-			return_location.street = '';
-			return_location.locality = '';
-			return_location.postcode = '';
-			return_location.region = '';
-			return_location.country = '';
 
-			var place = results[0];
-			var streetparts = [];
-			
-			for (var i = 0; i < place.address_components.length; i++) {
-				var addressComponent = place.address_components[i];
-				for (var j = 0; j < addressComponent.types.length; j++) {
-					var componentType = addressComponent.types[j];
-					switch (componentType) {
-						case 'country':
-							return_location.country = addressComponent.long_name;
-							break;
-						case 'administrative_area_level_1':
-							return_location.region = addressComponent.long_name;
-							break;
-						case 'locality':
-							return_location.locality = addressComponent.long_name;
-							break;
-						case 'street_address':
-							return_location.street = addressComponent.long_name;
-							break;
-						case 'postal_code':
-							return_location.postcode = addressComponent.long_name;
-							break;
-						case 'street_number':
-							streetparts.unshift(addressComponent.long_name);
-							break;
-						case 'route':
-							streetparts.push(addressComponent.long_name);
-							break;
+		else {
+			var places = [];
+
+			for (i=0; i<results.length; i++) {
+				place = results[i];
+				var streetparts = [];
+				var return_location = {};
+
+				for (var k = 0; k < place.address_components.length; k++) {
+					var addressComponent = place.address_components[k];
+					for (var j = 0; j < addressComponent.types.length; j++) {
+						var componentType = addressComponent.types[j];
+						switch (componentType) {
+							case 'country':
+								return_location.country = addressComponent.long_name;
+								break;
+							case 'administrative_area_level_1':
+								return_location.region = addressComponent.long_name;
+								break;
+							case 'locality':
+								return_location.locality = addressComponent.long_name;
+								break;
+							case 'street_address':
+								return_location.street = addressComponent.long_name;
+								break;
+							case 'postal_code':
+								return_location.postcode = addressComponent.long_name;
+								break;
+							case 'street_number':
+								streetparts.unshift(addressComponent.long_name);
+								break;
+							case 'route':
+								streetparts.push(addressComponent.long_name);
+								break;
+						}
 					}
 				}
+
+				if (return_location.street === '' && streetparts.length > 0) {
+					return_location.street = streetparts.join(' ');
+				}
+
+				return_location.point = new mxn.LatLonPoint(place.geometry.location.lat(), place.geometry.location.lng());
+
+				places.push(return_location);
 			}
-			
-			if (return_location.street === '' && streetparts.length > 0) {
-				return_location.street = streetparts.join(' ');
-			}		
-			
-			return_location.point = new mxn.LatLonPoint(place.geometry.location.lat(), place.geometry.location.lng());
-			
-			this.callback(return_location);
+
+			if (this.row_limit <= 1) {
+				this.callback(places[0]);
+			}
+			else {
+				if (places.length > this.row_limit) {
+					places.length = this.row_limit;
+				}
+				this.callback(places);
+			}
 		}
 	}
 }
+
 });

source/mxn.microsoft7.geocoder.js
@@ -1,52 +1,74 @@
-mxn.register('microsoft7', {	
-	Geocoder: {
+mxn.register('microsoft7', {
 
-		init: function(){
+Geocoder: {
+	init: function() {
 
-		},
+	},
 
-		geocode: function(query){
-			var self = this;
-			var _address = '';
-			var is_reverse = false;
-			if (typeof(query) == 'object') {
-				// query is a LatLonPoint object (reverse geocode)
-				if (query.hasOwnProperty('lat') && query.hasOwnProperty('lon')) {
-					_address = query.lat + ',' + query.lon;
-				}
-				// query is an address object
-				else {
-					_address = [ query.street, query.locality, query.region, query.country ].join(',');
-				}
+	row_limit: int = 1,
+
+	geocode: function(query, rowlimit) {
+		this.row_limit = rowlimit || 1; //default to one result
+		var _address = '';
+		var is_reverse = false;
+		if (typeof(query) == 'object') {
+			// query is a LatLonPoint object (reverse geocode)
+			if (query.hasOwnProperty('lat') && query.hasOwnProperty('lon')) {
+				_address = query.lat + ',' + query.lon;
 			}
-			// query is an address string
-			else{
-				_address = query;
+			// query is an address object
+			else {
+				_address = [ query.street, query.locality, query.region, query.country ].join(',');
 			}
-			jsonp_callback_context = this;
-			var searchRequest = 'http://dev.virtualearth.net/REST/v1/Locations/' + _address + '?output=json&jsonp=jsonp_callback_context.geocode_callback&key=' + microsoft_key;
-			var mapscript = document.createElement('script'); 
-			mapscript.type = 'text/javascript'; 
-			mapscript.src = searchRequest; 
-			document.body.appendChild(mapscript);
-		},
+		}
+		// query is an address string
+		else{
+			_address = query;
+		}
 
-		geocode_callback: function(results){
-			if (results.statusDescription != 'OK') {
-				this.error_callback(results.statusDescription);
+		jsonp_callback_context = this;
+		var searchRequest = 'http://dev.virtualearth.net/REST/v1/Locations/' + _address + '?output=json&jsonp=jsonp_callback_context.geocode_callback&key=' + microsoft_key;
+		var mapscript = document.createElement('script'); 
+		mapscript.type = 'text/javascript'; 
+		mapscript.src = searchRequest; 
+		document.body.appendChild(mapscript);
+	},
+
+	geocode_callback: function(results) {
+		if (results.statusDescription != 'OK') {
+			this.error_callback(results.statusDescription);
+		}
+
+		else {
+			var places = [];
+
+			for (i=0; i<results.resourceSets[0].resources.length; i++)
+			{
+				place = results.resourceSets[0].resources[i];
+
+				var return_location = {
+					street: place.address.addressLine,
+					locality: place.address.locality,
+					postcode: place.address.postalCode,
+					region: place.address.adminDistrict,
+					country: place.address.countryRegion,
+					point: new mxn.LatLonPoint(place.point.coordinates[0], place.point.coordinates[1])
+				};
+				
+				places.push(return_location);
+			}
+
+			if (this.row_limit <= 1) {
+				this.callback(places[0]);
 			}
 			else {
-				var topResult = results.resourceSets[0].resources[0];
-				var return_location = topResult ? {
-					street: topResult.address.addressLine,
-					locality: topResult.address.locality,
-					postcode: topResult.address.postalCode,
-					region: topResult.address.adminDistrict,
-					country: topResult.address.countryRegion,
-					point: new mxn.LatLonPoint(topResult.point.coordinates[0], topResult.point.coordinates[1])			
-				} : null;
-				this.callback(return_location);
+				if (places.length > this.row_limit) {
+					places.length = this.row_limit;
+				}
+				this.callback(places);
 			}
 		}
 	}
+}
+
 });

source/mxn.ovi.geocoder.js
@@ -5,7 +5,7 @@ Geocoder: {
 	init: function() {
 		var me = this;
 		var ovi_geocoder;
-		
+
 		if (typeof ovi.mapsapi.search.Manager === 'undefined') {
 			throw new Error(api + ' map script not imported');
 		}
@@ -18,20 +18,20 @@ Geocoder: {
 		});
 		this.geocoders[this.api] = ovi_geocoder;
 	},
-	
-	geocode: function(address){
+
+	geocode: function(address, rowlimit){
 		var ovi_geocoder = this.geocoders[this.api];
-		
+		this.row_limit = rowlimit || 1; //default to one result
 		ovi_geocoder.geocode(address);
 	},
-	
+
 	geocode_callback: function(response, status){
 		var ovi_geocoder = this.geocoders[this.api];
 
 		if (status == "failed") {
 			var error_cause = ovi_geocoder.getErrorCause();
 			var error_status = "";
-			
+
 			if (error_cause.type) {
 				error_status = error_cause.type;
 				if (error_cause.subtype) {
@@ -41,29 +41,33 @@ Geocoder: {
 					error_status += ", " + error_cause.message;
 				}
 			}
-			
+
 			else {
 				error_status = "Geocoding failure";
 			}
-			
+
 			this.error_callback(error_status);
 		}
-		
+
 		else if (status == "finished") {
-			var return_location = {};
-			var street_components = [];
-			var locality_components = [];
-			var region_components = [];
+			var places = [];
 
-			return_location.street = '';
-			return_location.locality = '';
-			return_location.postcode = '';
-			return_location.region = '';
-			return_location.country = '';
+			for (i=0; i<response.length; i++) {
+				place = response[i];
 
-			if (response.length > 0) {
-				var address = response[0].address;
-				var coords = response[0].displayPosition;
+				var return_location = {};
+				var street_components = [];
+				var locality_components = [];
+				var region_components = [];
+
+				return_location.street = '';
+				return_location.locality = '';
+				return_location.postcode = '';
+				return_location.region = '';
+				return_location.country = '';
+
+				var address = place.address;
+				var coords = place.displayPosition;
 
 				if (address.street) {
 					street_components.push(address.street);
@@ -105,9 +109,22 @@ Geocoder: {
 				}
 
 				return_location.point = new mxn.LatLonPoint(coords.latitude, coords.longitude);
-				this.callback(return_location);
+
+				places.push(return_location);
+			}
+
+			if (this.row_limit <= 1) {
+				this.callback(places[0]);
+			}
+
+			else {
+				if (places.length > this.row_limit) {
+					places.length = this.row_limit;
+				}
+				this.callback(places);
 			}
 		}
 	}
 }
+
 });

commit 3acd264dc67c30dc3a4b4fb71848e5ee525a1cad

Fix OpenLayers reverse geocoder broken by 6daf742 (pull request #202).

source/mxn.openlayers.geocoder.js
@@ -42,9 +42,13 @@ Geocoder: {
 	},
 
 	geocode_callback: function(results, rowlimit) {
-		if (results instanceof Array && !results.length) {
-			this.error_callback("Nominatim didn't recognize this address.");
-			return;
+		if (results instanceof Array) {
+			if (!results.length) {
+				this.error_callback("Nominatim didn't recognize this address.");
+				return;
+			}
+		} else {
+			var results = [results];
 		}
 
 		var place;